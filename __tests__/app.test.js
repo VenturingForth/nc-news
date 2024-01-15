c04089f87ded09a6cf3142627ea35100b4272863
VenturingForth <opilapro@gmail.com>
2024-01-15 14:25:38 +0000

    Bad API Filepath error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,17 @@ beforeEach(() => seed(testData));
 
 afterAll(() => db.end());
 
+describe("General API error handling", () => {
+    test("404: Unavailable Route - Should display custom error message if no API path found", () => {
+        return request(app)
+        .get('/api/invalid')
+        .expect(404)
+        .then((res) => {
+            expect(res.body.msg).toBe("Invalid Endpoint");
+        })
+    })
+})
+
 describe("GET /api", () => {
     describe("/topics", () => {
         test("200: Should respond with an array of topics with slugs and descriptions", () => {
--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 
-const { getTopics } = require("./controllers/topics-controllers.js")
-
-app.use(express.json());
+const { getTopics } = require("./controllers/topics-controllers.js");
 
 app.get('/api/topics', getTopics);
 
-//Bad Request (400) Handling
+//Unavailable Route (404) Error Handling
+app.all('*', (req, res) => {
+    res.status(404).send({msg: "Invalid Endpoint"});
+})
 
 //Internal Error (500) Handling
 app.use((err, req, res, next) => {
-    res.status(500).send({msg: "Internal error: check your code."})
+    res.status(500).send({msg: "Internal error: check your code."});
 })
 
 module.exports = app;
