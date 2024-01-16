f7c17e8361c3f0bdaca6f230f293393de8da4957
VenturingForth <opilapro@gmail.com>
2024-01-16 15:30:11 +0000

    Added fetchArticles API with testing for sorting and comment_count query

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,5 +1,19 @@
 const db = require("../db/connection.js");
 
+module.exports.fetchArticles = () => {
+    return db.query(`
+        SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, (
+            SELECT COUNT(*)::int FROM comments WHERE comments.article_id = articles.article_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.author, articles.title, articles.article_id
+        ORDER BY created_at DESC;
+        `).
+    then(({rows}) => {
+        return rows;
+    })
+}
+
 module.exports.fetchArticleById = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then(({rows}) => {
